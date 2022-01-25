22416886146e6460b8d6daf96fa45b2c4551c107
William Busby <williambusby1999@gmail.com>
2022-01-25 15:32:02 +0000

    Working with refactored javascript

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
+// Defining global variables used within javascript functions
+
 let currentNumber = "";
 let numberVals = [];
 let operationVals = [];
-let outputNumber;
+let outputNumber = 0;
+let fullString = "";
+
+// Getting information from HTML document
 
 const numberButtonsArr = [...document.getElementsByClassName("numbers")];
 const functionButtonsArr = [...document.getElementsByClassName("functions")];
-
+const additionalButtons = [...document.getElementsByClassName("lightgrey")];
 const allButtons = document.querySelectorAll("button");
 const equalsButton = document.getElementById("equal");
-const clearButton = document.getElementById("remove");
-const plusMinus = document.getElementById("plusMinus");
-const percent = document.getElementById("percent");
-const additionalButtons = [...document.getElementsByClassName("lightgrey")];
+
+// Function to display output to main display & run once on open page
 
 const displayOutput = (numberToDisplay) => {
-  document.getElementById("output__number").innerHTML = numberToDisplay.toLocaleString();
+  document.getElementById("output__big").innerHTML = numberToDisplay.toLocaleString();
 }
 
 displayOutput(0);
 
+// Function to display full equation
+
+const smallDisplay = () => {
+  document.getElementById("output__small").innerHTML = fullString;
+}
+
+// Function to clear global variables in input array
+
 const clearValues = (valuesToClear) => {
   for(let i = 0; i < valuesToClear.length; i++) {
-    if((valuesToClear[i]) === currentNumber) {
+    if((valuesToClear[i]) == "currentNumber") {
       currentNumber = "";
-    } else if((valuesToClear[i]) === numberVals) {
+    } else if((valuesToClear[i]) == "numberVals") {
       numberVals = [];
-    } else if((valuesToClear[i]) === operationVals) {
+    } else if((valuesToClear[i]) == "operationVals") {
       operationVals = [];
-    } else if ((valuesToClear[i]) === outputNumber) {
+    } else if ((valuesToClear[i]) == "outputNumber") {
       outputNumber = 0;
+    } else if((valuesToClear[i]) == "fullString") {
+      fullString = "";
     }
   }
 }
+
+// Adds preventDefault to all buttons
 
 for(let i= 0; i< allButtons.length; i++) {
   allButtons[i].addEventListener("click", (event) => {
@@ -39,29 +54,43 @@ for(let i= 0; i< allButtons.length; i++) {
   })
 }
 
+// Adds number addEventListener to all number buttons
+
 for (let i = 0; i < numberButtonsArr.length; i++) {
   numberButtonsArr[i].addEventListener("click", (event) => {
     if(currentNumber.includes(".") && numberButtonsArr[i].innerHTML == ".") {
       alert("You can't have two decimal points in one number!")
     } else {
       currentNumber += numberButtonsArr[i].innerHTML;
+      fullString += numberButtonsArr[i].innerHTML;
       displayOutput(currentNumber);
+      smallDisplay();
     }
   })
 }
+
+// Adds function addEventListener to all function buttons
 
 for (let i = 0; i < functionButtonsArr.length; i++) {
   functionButtonsArr[i].addEventListener("click", (event) => {
     numberVals.push(currentNumber);
     operationVals.push(functionButtonsArr[i].innerHTML);
-    clearValues(currentNumber);
+    fullString += functionButtonsArr[i].innerHTML;
+    clearValues(["currentNumber"])
+    smallDisplay();
+    displayOutput(0);
   })
 }
 
+// Adds function addEventListener to equals button
+
 equalsButton.addEventListener("click", (event) => {
   calculate();
+  smallDisplay();
   displayOutput(outputNumber);
 })
+
+// Calculate function used within equals
 
 const calculate = () => {
   numberVals.push(currentNumber);
@@ -77,21 +106,24 @@ const calculate = () => {
       outputNumber = outputNumber * Number(numberVals[i]);
     } else if(operationVals[i-1] == "/" && numberVals[i] == "0") {
       alert("Can't divide by 0!");
-      clearValues([outputNumber, currentNumber, numberVals, operationVals]);
+      clearValues(["outputNumber", "currentNumber", "numberVals", "operationVals", "fullString"]);
     }
   }
-  clearValues([currentNumber, numberVals, operationVals]);
+  clearValues(["currentNumber", "numberVals", "operationVals", "fullString"]);
 }
+
+// Adds function to each of the additional function buttons (CE, +/-, %)
 
 for(let i = 0; i < additionalButtons.length; i++) {
   additionalButtons[i].addEventListener("click", (event) => {
-    if(additionalButtons[i] == clearButton) {
-      clearValues([currentNumber,outputNumber,numberVals,operationVals]);
-    } else if(additionalButtons[i] == percent) {
-      outputNumber = document.getElementById("output__number").innerHTML * 0.01;
-    } else if(additionalButtons[i] == plusMinus) {
-      outputNumber = document.getElementById("output__number").innerHTML * -1;
+    if(additionalButtons[i].id == "remove") {
+      clearValues(["currentNumber", "outputNumber", "numberVals", "operationVals", "fullString"]);
+    } else if(additionalButtons[i].id == "percent") {
+      outputNumber = document.getElementById("output__big").innerHTML * 0.01;
+    } else if(additionalButtons[i].id == "plusMinus") {
+      outputNumber = document.getElementById("output__big").innerHTML * -1;
     }
     displayOutput(outputNumber);
+    smallDisplay();
   })
 }
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -8,48 +8,56 @@ function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.
 
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
+// Defining global variables used within javascript functions
 var currentNumber = "";
 var numberVals = [];
 var operationVals = [];
-var outputNumber;
+var outputNumber = 0;
+var fullString = ""; // Getting information from HTML document
 
 var numberButtonsArr = _toConsumableArray(document.getElementsByClassName("numbers"));
 
 var functionButtonsArr = _toConsumableArray(document.getElementsByClassName("functions"));
 
-var allButtons = document.querySelectorAll("button");
-var equalsButton = document.getElementById("equal");
-var clearButton = document.getElementById("remove");
-var plusMinus = document.getElementById("plusMinus");
-var percent = document.getElementById("percent");
-
 var additionalButtons = _toConsumableArray(document.getElementsByClassName("lightgrey"));
 
+var allButtons = document.querySelectorAll("button");
+var equalsButton = document.getElementById("equal"); // Function to display output to main display & run once on open page
+
 var displayOutput = function displayOutput(numberToDisplay) {
-  document.getElementById("output__number").innerHTML = numberToDisplay.toLocaleString();
+  document.getElementById("output__big").innerHTML = numberToDisplay.toLocaleString();
 };
 
-displayOutput(0);
+displayOutput(0); // Function to display full equation
+
+var smallDisplay = function smallDisplay() {
+  document.getElementById("output__small").innerHTML = fullString;
+}; // Function to clear global variables in input array
+
 
 var clearValues = function clearValues(valuesToClear) {
   for (var i = 0; i < valuesToClear.length; i++) {
-    if (valuesToClear[i] === currentNumber) {
+    if (valuesToClear[i] == "currentNumber") {
       currentNumber = "";
-    } else if (valuesToClear[i] === numberVals) {
+    } else if (valuesToClear[i] == "numberVals") {
       numberVals = [];
-    } else if (valuesToClear[i] === operationVals) {
+    } else if (valuesToClear[i] == "operationVals") {
       operationVals = [];
-    } else if (valuesToClear[i] === outputNumber) {
+    } else if (valuesToClear[i] == "outputNumber") {
       outputNumber = 0;
+    } else if (valuesToClear[i] == "fullString") {
+      fullString = "";
     }
   }
-};
+}; // Adds preventDefault to all buttons
+
 
 for (var i = 0; i < allButtons.length; i++) {
   allButtons[i].addEventListener("click", function (event) {
     event.preventDefault();
   });
-}
+} // Adds number addEventListener to all number buttons
+
 
 var _loop = function _loop(_i) {
   numberButtonsArr[_i].addEventListener("click", function (event) {
@@ -57,31 +65,39 @@ var _loop = function _loop(_i) {
       alert("You can't have two decimal points in one number!");
     } else {
       currentNumber += numberButtonsArr[_i].innerHTML;
+      fullString += numberButtonsArr[_i].innerHTML;
       displayOutput(currentNumber);
+      smallDisplay();
     }
   });
 };
 
 for (var _i = 0; _i < numberButtonsArr.length; _i++) {
   _loop(_i);
-}
+} // Adds function addEventListener to all function buttons
+
 
 var _loop2 = function _loop2(_i2) {
   functionButtonsArr[_i2].addEventListener("click", function (event) {
     numberVals.push(currentNumber);
     operationVals.push(functionButtonsArr[_i2].innerHTML);
-    clearValues(currentNumber);
+    fullString += functionButtonsArr[_i2].innerHTML;
+    clearValues(["currentNumber"]);
+    smallDisplay();
+    displayOutput(0);
   });
 };
 
 for (var _i2 = 0; _i2 < functionButtonsArr.length; _i2++) {
   _loop2(_i2);
-}
+} // Adds function addEventListener to equals button
+
 
 equalsButton.addEventListener("click", function (event) {
   calculate();
+  smallDisplay();
   displayOutput(outputNumber);
-});
+}); // Calculate function used within equals
 
 var calculate = function calculate() {
   numberVals.push(currentNumber);
@@ -98,24 +114,26 @@ var calculate = function calculate() {
       outputNumber = outputNumber * Number(numberVals[_i3]);
     } else if (operationVals[_i3 - 1] == "/" && numberVals[_i3] == "0") {
       alert("Can't divide by 0!");
-      clearValues([outputNumber, currentNumber, numberVals, operationVals]);
+      clearValues(["outputNumber", "currentNumber", "numberVals", "operationVals", "fullString"]);
     }
   }
 
-  clearValues([currentNumber, numberVals, operationVals]);
-};
+  clearValues(["currentNumber", "numberVals", "operationVals", "fullString"]);
+}; // Adds function to each of the additional function buttons (CE, +/-, %)
+
 
 var _loop3 = function _loop3(_i4) {
   additionalButtons[_i4].addEventListener("click", function (event) {
-    if (additionalButtons[_i4] == clearButton) {
-      clearValues([currentNumber, outputNumber, numberVals, operationVals]);
-    } else if (additionalButtons[_i4] == percent) {
-      outputNumber = document.getElementById("output__number").innerHTML * 0.01;
-    } else if (additionalButtons[_i4] == plusMinus) {
-      outputNumber = document.getElementById("output__number").innerHTML * -1;
+    if (additionalButtons[_i4].id == "remove") {
+      clearValues(["currentNumber", "outputNumber", "numberVals", "operationVals", "fullString"]);
+    } else if (additionalButtons[_i4].id == "percent") {
+      outputNumber = document.getElementById("output__big").innerHTML * 0.01;
+    } else if (additionalButtons[_i4].id == "plusMinus") {
+      outputNumber = document.getElementById("output__big").innerHTML * -1;
     }
 
     displayOutput(outputNumber);
+    smallDisplay();
   });
 };
 
